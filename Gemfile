d1d275ee010b3fe079716e8e3ada1b8cf94e9b97
MXZTY <mtyso088@mtroyal.ca>
2019-10-17 17:19:13 -0600

    installed bootstrap

--- Gemfile
+++ Gemfile
@@ -9,6 +9,9 @@ gem 'rails', '~> 6.0.0'
 # gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
+
+gem 'bootstrap-sass', '~> 3.4.1'
+gem 'jquery-rails'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,14 @@
+// this is a manifest file that will be compiles into application.js, which will include all the files listed below 
+// 
+// Any javascript/coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts, 
+// or any plugins vendor/assets/javascripts directory can be referenced here using a relative path
+
+// It is not advisable to add code directly here, but if you do, it will appear at the bottom of the compiled file. 
+
+// read sprockets README for details about supported directorives. 
+
+//= require jquery_ujs
+//= require jquery
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require_tree .
